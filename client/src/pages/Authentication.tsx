c8116a1cf768e7511825bf7bffa8be4da90b556a
munaimun <munaimunz@gmail.com>
2024-11-26 18:22:04 +0600

    test commit

--- client/src/pages/Authentication.tsx
+++ client/src/pages/Authentication.tsx
@@ -4,6 +4,7 @@ import SignUp from "../components/SignUp";
 const Authentication = () => {
   return (
     <div className="max-w-screen-xl mx-auto flex flex-wrap justify-around mt-4">
+      <p>Test commit</p>
       <SignInForm />
       <SignUp />
     </div>
