b2c8192bbe02a6edf5619c1d88613e04b94abfb9
munaimun <munaimunz@gmail.com>
2024-11-28 15:04:37 +0600

    working on order hsitory

--- client/src/pages/Authentication.tsx
+++ client/src/pages/Authentication.tsx
@@ -4,7 +4,6 @@ import SignUp from "../components/SignUp";
 const Authentication = () => {
   return (
     <div className="max-w-screen-xl mx-auto flex flex-wrap justify-around mt-4">
-      <p>Test commit</p>
       <SignInForm />
       <SignUp />
     </div>
