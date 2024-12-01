fba55cbf6bcde6431a0bce2b23e1d973995d8ebe
munaimun <munaimunz@gmail.com>
2024-12-01 18:28:59 +0600

    added functionality to redirect in particular product

--- client/src/components/ProductCardSideNav.tsx
+++ client/src/components/ProductCardSideNav.tsx
@@ -1,16 +1,19 @@
 import { useEffect, useState } from "react";
 import toast from "react-hot-toast";
 import { FaRegEye, FaRegStar, FaStar } from "react-icons/fa";
-import { LuArrowLeftRight } from "react-icons/lu";
+import {} from "react-icons/lu";
 
 import { ProductProps } from "../../type";
 import { store } from "../lib/store";
+import { useNavigate } from "react-router-dom";
 
 const ProductCardSideNav = ({ product }: { product?: ProductProps }) => {
   const { addToFavorite, favoriteProduct } = store();
   const [existingProduct, setExistingProduct] = useState<ProductProps | null>(
     null
   );
+
+  const navigate = useNavigate();
 
   useEffect(() => {
     const availableItem = favoriteProduct.find(
@@ -30,6 +33,14 @@ const ProductCardSideNav = ({ product }: { product?: ProductProps }) => {
       });
     }
   };
+
+  // receiving the product as props, then redirecting to the particular product using its id
+  const handleClick = () => {
+    if (product?._id) {
+      navigate(`/product/${product._id}`);
+    }
+  };
+
   return (
     <div className="absolute right-1 top-1 flex flex-col gap-1 transition translate-x-12 group-hover:translate-x-0 duration-300">
       <span
@@ -38,11 +49,9 @@ const ProductCardSideNav = ({ product }: { product?: ProductProps }) => {
       >
         {existingProduct ? <FaStar /> : <FaRegStar />}
       </span>
+
       <span className="w-11 h-11 inline-flex text-black text-lg items-center justify-center rounded-full hover:text-white hover:bg-black duration-200">
-        <LuArrowLeftRight />
-      </span>
-      <span className="w-11 h-11 inline-flex text-black text-lg items-center justify-center rounded-full hover:text-white hover:bg-black duration-200">
-        <FaRegEye />
+        <FaRegEye onClick={handleClick} />
       </span>
     </div>
   );
