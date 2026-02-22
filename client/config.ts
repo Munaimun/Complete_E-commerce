b83b8c3eee53b9fca258ee07a92f368e74043509
munaimun <munaimunz@gmail.com>
2026-02-23 02:06:35 +0600

    fix: remove checkConfig and use VITE_SERVER directly

--- client/config.ts
+++ client/config.ts
@@ -1,18 +1,26 @@
+// interface Config {
+//   baseUrl: string;
+// }
+
+// const checkConfig = (server: string): Config => {
+//   switch (server) {
+//     case "production":
+//       return { baseUrl: "https://complete-e-commerce.vercel.app" };
+//     case "local":
+//       return { baseUrl: "http://localhost:8000" };
+//     default:
+//       // Provide a fallback configuration
+//       return { baseUrl: "http://default-url.com" };
+//   }
+// };
+
+// export const selectServer = import.meta.env.VITE_SERVER;
+// export const config = checkConfig(selectServer) as Config;
+
 interface Config {
   baseUrl: string;
 }
 
-const checkConfig = (server: string): Config => {
-  switch (server) {
-    case "production":
-      return { baseUrl: "https://complete-e-commerce.vercel.app" };
-    case "local":
-      return { baseUrl: "http://localhost:8000" };
-    default:
-      // Provide a fallback configuration
-      return { baseUrl: "http://default-url.com" };
-  }
+export const config: Config = {
+  baseUrl: import.meta.env.VITE_SERVER as string,
 };
-
-export const selectServer = import.meta.env.VITE_SERVER;
-export const config = checkConfig(selectServer) as Config;
